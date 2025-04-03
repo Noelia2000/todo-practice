a1344e7e1d168b1eb61edc4a05dd03ac9c671a80
Noe <noelia2000vane@gmail.com>
2025-04-03 16:50:51 -0400

    mark as completed button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { JSX, useState } from "react"
 import { Todos } from "./components/Todos"
+import {  type TodoId, type Todo as TodoType } from "./types"
+
 const mockTodos = [
   {
     id: '1',
@@ -21,15 +23,30 @@ const mockTodos = [
 const App =(): JSX.Element =>{
   const [todos, setTodos]=useState(mockTodos)
 
-  const handleRemove= (id: string): void=>{
+  const handleRemove= ({ id }: TodoId): void=>{
     const newTodos = todos.filter(todo => todo.id !== id )
     setTodos(newTodos)
+  }
 
+  const handleCompleted = (
+    { id, completed } :Pick<TodoType, 'id' | 'completed'>
+  ): void=>{
+    const newTodos= todos.map(todo=>{
+      if (todo.id == id){
+        return{
+          ...todo,
+          completed
 
+        }
+      }
+      return todo
+    })
+    setTodos(newTodos)
   }
   return(
   <div className="todoapp">
   <Todos
+  onToggleCompleteTodo={handleCompleted}
   onRemoveTodo={handleRemove}
    todos={todos}/>
   </div>
